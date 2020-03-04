d16340652cf88d232893b978e1002e5b4a657471
Joshua <jbkunst@gmail.com>
2020-03-03 23:34:55 -0300

    trying some :checkered_flag:

--- global.R
+++ global.R
@@ -1,7 +1,30 @@
 library(shiny)
 library(shinydashboard)
 library(highcharter)
+library(tradestatistics)
+library(purrr)
+library(dplyr)
+library(countrycode)
+library(shinyWidgets)
 
 mapwrdl <- hcmap(showInLegend = FALSE) %>%
   hc_size(height = "90vh") %>%
   hc_elementId("worldmap")
+
+countries <- tradestatistics::ots_countries %>% 
+  mutate(
+    iso2 = countrycode(country_iso, origin = "iso3c", destination = "iso2c")    
+  ) %>% 
+  filter(!is.na(iso2))
+
+cntnt <- countries %>% 
+  select(country = country_name_english, iso2) %>% 
+  pmap_chr(function(country = "Australia", iso2 = "au") {
+    
+    urlflag <- paste0("https://cdn.rawgit.com/lipis/flag-icon-css/master/flags/4x3/", tolower(iso2), ".svg")
+
+    as.character(HTML(paste(tags$img(src = urlflag, width=20, height=15), country)))
+    
+  })
+
+str(cntnt)
--- ui.R
+++ ui.R
@@ -6,7 +6,13 @@ dashboardPage(
             style = "position: absolute; width: 100%; left: 0; z-index: 0; height: 90vh;", 
             mapwrdl
         ),
-        selectInput("country", NULL, c("Chile", "Argentina")),
+        # selectInput("country", NULL, c("Chile", "Argentina")),
+        pickerInput(
+            "country", NULL, multiple = FALSE,
+            choices = pull(countries, country_name_english),
+            choicesOpt = list(content =  cntnt)
+            ),
+        
         fluidRow(
             valueBox(10 * 2, "New Orders", icon = icon("credit-card")),
             valueBox(10 * 2, "New Orders", icon = icon("credit-card")),
