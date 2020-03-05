0e7b941bdbebd55d18c5f3e8f16d5fc689302b8d
Joshua <jbkunst@gmail.com>
2020-03-05 01:05:03 -0300

    wip data_yr data_yrc

--- R/00-download-data.R
+++ R/00-download-data.R
@@ -7,8 +7,10 @@ plan(multiprocess(workers = 6))
 
 tradestatistics::ots_countries$country_iso %>% 
   str_subset("-", negate = TRUE) %>% 
-  setdiff("all") %>% 
-  future_map(function(ciso = "chl"){
+  setdiff(c("all", "arb", "tmp")) %>% 
+  map(function(ciso = "arb"){
+    
+    message(ciso)
     
     fout <- paste0("data/country/", ciso, ".rds")
     
@@ -25,32 +27,42 @@ tradestatistics::ots_countries$country_iso %>%
     
     saveRDS(d, fout, compress = "xz")
     
-  }, .progress = TRUE)
+  })
 
-data <- dir("data/country/", full.names = TRUE) %>% 
+data_yrc <- dir("data/country/", full.names = TRUE) %>% 
   map_df(readRDS)
 
-data_2000 <- data %>% 
-  filter(year == 2000) %>% 
-  mutate(export_value_usd = replace_na(export_value_usd, 0))
+data_yrc <- data_yrc %>% 
+  mutate_if(is.numeric, replace_na, 0)
 
-rca_2000 <- balassa_index(data_2000, country = "reporter_iso", product = "community_name", value = "export_value_usd", discrete = TRUE)
+data_yr <- data_yrc %>% 
+  distinct(year) %>% 
+  pull() %>% 
+  map_df(function(y = 2000){
+    
+    message(y)
+    
+    dux <- data_yrc %>% 
+      filter(year == y)
+    
+    rca <- balassa_index(dux, country = "reporter_iso", product = "community_name", value = "export_value_usd", discrete = TRUE)
+    
+    com <- complexity_measures(rca)
+    
+    dot <- tibble(
+      reporter_iso = names(com$complexity_index_country),
+      complexity_index_country = com$complexity_index_country
+      )
+    
+    dux %>% 
+      group_by(reporter_iso) %>% 
+      summarise_at(vars(export_value_usd, import_value_usd), sum) %>% 
+      mutate(year = y) %>% 
+      left_join(dot, by = "reporter_iso") %>% 
+      select(year, everything())
+    
+  })
 
-com_2000 <- complexity_measures(rca_2000)
 
-com_2000
-
-
-data_2000
-
-data %>% 
-  group_by(year) %>% 
-  economiccomplexity::complexity_measures()
-
-data %>% 
-  group_by(year, reporter_iso) %>% 
-  summarise_at(vars(export_value_usd, import_value_usd), sum, na.rm = TRUE) %>% 
-  ungroup()
-
-
-economiccomplexity::
+data_yrc
+data_yr
